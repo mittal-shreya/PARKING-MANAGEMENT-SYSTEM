add6929ffefd4bafa610c5d594c3054d15d91f31
Shreya Mittal <117579735+mittal-shreya@users.noreply.github.com>
2024-04-21 23:44:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/parkingdb'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/dbname' //enter your details
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
